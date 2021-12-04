631098c9bb6419d7bbfe3eadc923dbc26b119b2a
pppcccc <83036778+pppcccc@users.noreply.github.com>
2021-12-04 16:44:21 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ my_client = pymongo.MongoClient("mongodb://localhost:27017/")
 my_db = my_client["SE2_Friend_Finder"]
 user_account_db = my_db["user_accounts"]
 user_profile_db = my_db["user_profiles"]
+friend_db = my_db["friend_relationships"]
 key = open('encrypt.key', 'rb').read()
 f = Fernet(key)
 
@@ -66,10 +67,29 @@ def verify_credentials(username, password):
     query = {"username": username}
     mydoc = user_account_db.find(query)
     for x in mydoc:
-        if  password == f.decrypt(x['password']).decode():
+        if password == f.decrypt(x['password']).decode():
             verified = True
 
     return verified
+
+def check_friendship(username1, username2):
+    status = 0
+    query = {"username1": username1, "username2": username2}
+    mydoc = friend_db.find(query)
+    for x in mydoc:
+        if x:
+            status = x['status']
+
+    return status
+
+def create_a_friendship(username1, username2):
+    try:
+        friend_dict = {"username1": username1, "username2": username2, "status": 1}
+        friend_db.insert_one(friend_dict)
+        return True
+
+    except:
+        return False
 
 @app.route('/')
 def index():
@@ -83,25 +103,48 @@ def index():
     except:
         return render_template('views/index.html', authed=False, username='')
 
+
+@app.route('/friends')
+def friends():
+    # check if user logged in has received friend requests
+    # and see if they've sent any
+    # also check for tuples where they are friends (status=2)
+    return render_template_string("friends")
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['searchquery']
+    # find the query if its in bio/name/interests of all users
+
+    return render_template('views/searchresults.html', results=results)
+
+@app.route('/create_friendship', methods=['POST'])
+def create_friendship():
+    users = request.args.get('users').split(' ')
+    if create_a_friendship(users[0], users[1]):
+        return redirect(request.referrer)
+
+    else:
+        return render_template('views/error.html', err='Could not add friend.')
+
+
 @app.route('/all-profiles')
 def get_all():
-    try:
-        username = request.cookies.get('userID')
-        user_profile_list = []
-        cursor = user_profile_db
-        for x in cursor.find():
-            pro = user_profile(x['username'], x['name'], x['age'], x['major'], x['year'], x['biography'], x['interests'])
-            user_profile_list.append(pro)
+    username = request.cookies.get('userID')
+    user_profile_list = []
+    cursor = user_profile_db
+    for x in cursor.find():
+        pro = user_profile(x['username'], x['name'], x['age'], x['major'], x['year'], x['biography'], x['interests'])
+        user_profile_list.append(pro)
 
 
-        if username:
-            return render_template('views/allprofiles.html', authed=True, username=username, user_profile_list=user_profile_list)
+    if username:
+        return render_template('views/allprofiles.html', check_friendship=check_friendship, authed=True, auth=username, user_profile_list=user_profile_list)
 
-        else:
-            return render_template('views/allprofiles.html', authed=False, user_profile_list=user_profile_list)
-
-    except:
+    else:
         return render_template('views/allprofiles.html', authed=False, user_profile_list=user_profile_list)
+
 
 
 @app.route('/create_profile', methods=['POST'])
@@ -130,16 +173,21 @@ def create_profile():
 @app.route('/create_account', methods=['POST'])
 def create():
     try:
-        username = request.form['username']
-        password = f.encrypt(request.form['password'].encode())
-        if not find_account(username):
-            account_dict = {"username": username, "password": password}
-            user_account_db.insert_one(account_dict)
-            resp = make_response(redirect(f'/profile/{username}'))
-            resp.set_cookie('userID', username)
-            return resp
+        username = request.form['username'].replace(' ', '')
+        if len(username) > 1:
+            password = f.encrypt(request.form['password'].encode())
+            if not find_account(username):
+                account_dict = {"username": username, "password": password}
+                user_account_db.insert_one(account_dict)
+                resp = make_response(redirect(f'/profile/{username}'))
+                resp.set_cookie('userID', username)
+                return resp
+
+            else:
+                return render_template('views/error.html', err='Username already taken.')
+
         else:
-            return render_template('views/error.html', err='Username already taken.')
+            return render_template('views/error.html', err='Please enter a non-spaced username.')
 
     except Exception as error:
         print(f'ERROR: {str(error)}')
@@ -163,31 +211,36 @@ def login():
         return render_template('views/error.html', err='User/password combo entered does not exist.')
 
 
-@app.route('/setup_profile')
+@app.route('/setup-profile')
 def setup_profile():
     try:
         auth = request.cookies.get('userID')
         if auth:
             return render_template('views/createprofile.html', authed=True, username=auth)
-    except Exception:
+    except Exception as profile_err:
+        print(profile_err)
         return render_template('views/error.html', err='Please log in to edit profile.')
 
 
 @app.route('/profile/<username>')
 def get_user(username):
     setup = False
+    authedself = False
     authed = False
     auth = request.cookies.get('userID')
     if auth == username:
+        authedself = True
+
+    if auth:
         authed = True
 
     user = find_user_profile(username)
 
     if user:
-        return render_template('views/profile.html', user_profile=user, authed=authed)
+        return render_template('views/profile.html', check_friendship=check_friendship, auth=auth, authedself=authedself , user_profile=user, authed=authed)
     else:
         if authed:
-            return redirect('/setup_profile')
+            return redirect('/setup-profile')
         else:
             return render_template('views/error.html', err='Profile does not exist.')
 
@@ -201,13 +254,17 @@ def logout():
 @app.route('/edit_profile')
 def edit_profile():
     auth = request.cookies.get('userID')
-    user = find_user_profile(auth)
-    interests = ', '.join(user.interests)
-    if user:
-        return render_template('views/editprofile.html', user_profile=user, interests=interests, authed=True)
+    if auth:
+        user = find_user_profile(auth)
+        interests = ', '.join(user.interests)
+        if user:
+            return render_template('views/editprofile.html', user_profile=user, interests=interests, authed=True)
+        else:
+            if authed:
+                return redirect('/setup-profile')
+
     else:
-        if authed:
-            return redirect('/setup_profile')
+        return render_template('views/error.html', err='Please log in to edit profile.')
 
 
 
